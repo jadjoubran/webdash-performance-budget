6504deb594ea4384b4f0566a2c723e303f6f708b
Jad Joubran <joubran.jad@gmail.com>
2018-02-05 21:23:08 +0100

    Proof of concept

--- api.js
+++ api.js
@@ -1,8 +1,22 @@
+const glob = require('glob');
+
 module.exports = {
     routes: {
         get: {
-            'package-json': (req, res) => {
+            'js-size': async (req, res) => {
+                const config = req.app.locals.getConfig();
 
+                const pattern = config.jsBudgetPath || "./dist/*.min.js";
+
+                const g = glob(pattern, { stat: true }, (er, files) => { });
+
+                let size = 0;
+                g.on('stat', function (path, stat) {
+                    size += stat.size;
+                })
+                g.on('end', () => {
+                    res.send({ size });
+                });
             }
         }
     }
